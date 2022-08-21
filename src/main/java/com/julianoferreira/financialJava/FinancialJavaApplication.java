a3b9600a8f7f9a777fe79b4e4469bb59bb4cb4f6
Juliano Ferreira <julianooen@hotmail.com>
2022-08-21 12:35:51 -0300

    first commit

---
+++ src/main/java/com/julianoferreira/financialJava/FinancialJavaApplication.java
@@ -0,0 +1,16 @@
+package com.julianoferreira.financialJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FinancialJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialJavaApplication.class, args);
+	}
+
+
+
+}
---
+++ src/main/java/com/julianoferreira/financialJava/controller/Controller.java
@@ -0,0 +1,50 @@
+package com.julianoferreira.financialJava.controller;
+
+import com.julianoferreira.financialJava.service.CompoundService;
+import com.julianoferreira.financialJava.service.SimpleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/calc")
+public class Controller {
+    @Autowired
+    private CompoundService compoundService;
+
+    @Autowired
+    private SimpleService simpleService;
+
+    @GetMapping("/texto")
+    public String texto(){
+        return"work";
+    }
+
+
+    @GetMapping("/compost")
+    public String compostCalc(@RequestParam(value = "capital", required = true) Double k,
+                              @RequestParam(value = "time", required = true) Integer t,
+                              @RequestParam(value = "tax", required = true) Double i) {
+        try {
+            return compoundService.setLoan(k, i, t);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error: invalid parameters!");
+        }
+    }
+
+    @GetMapping("/simple")
+    public String simpleCalc(@RequestParam(value = "capital", required = true) Double k,
+                             @RequestParam(value = "time", required = true) Integer t,
+                             @RequestParam(value = "tax", required = true) Double i) {
+        try {
+            return simpleService.setLoan(k, i, t);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error: invalid parameters!");
+        }
+    }
+
+}
